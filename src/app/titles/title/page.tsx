feebf55f07b1bfcd5e8377d55ddeb56da691d281
Gaston Cargnelutti <gastoncargnelutti@gmail.com>
2024-06-26 21:03:46 -0300

    feat: add css to title

--- src/app/titles/title/page.tsx
+++ src/app/titles/title/page.tsx
@@ -1,11 +1,20 @@
-
+import styles from '@/ui/title.module.css';
+import Link from 'next/link';
 interface ITitle {
   name: string;
 }
 
 const Title: React.FC<ITitle> = ({ name }) => {
+  const goTo = () => {
+    console.log('go to')
+  }
+
   return (
-    <div style={{background: "red", maxWidth: 150}}>{name}</div>
+    <Link href={'/'} className={styles.container}>
+      <h2 className={styles.text}>
+        {name}
+      </h2>
+    </Link>
   )
 }
 
